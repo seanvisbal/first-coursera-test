201105c747d0af19e430b591781f3b8ec0e0eb55
Sean <smvisbal@gmail.com>
2023-05-04 16:34:51 +0800

    rescan

---
+++ Assignments/Assignment 1/app.js
@@ -0,0 +1,45 @@
+(function (){
+    'use strict';
+ 
+    angular.module('LunchApp', [])
+    .controller('lunchController', LunchController);
+
+    LunchController.$inject = ['$scope'];
+    function LunchController($scope) {
+        $scope.items = "";
+        $scope.message = "";
+
+        $scope.checkIfTooMuch = function() {
+            // split items by comma and remove any empty or whitespace-only items
+            var itemsArray = $scope.items.split(",").filter(function(item) {
+                return item.trim() !== "";
+            });
+
+            // check number of items and set message accordingly
+            if (itemsArray.length === 0) {
+                $scope.message = "Please enter data first";
+            } else if (itemsArray.length <= 3) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too much!";
+            }
+        };
+    }
+ 
+})();
+
+
+
+
+
+
+
+
+
+   // angular.module('LunchApp', [])
+    // .controller('lunchController', LunchController);
+    
+    // LunchController.$inject = ['$scope'];
+    // function LunchController($scope) {
+
+    // }
